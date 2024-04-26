b4c7df50543afaa5aaf627d491c41aa7e5d0d5d5
Tobias Weber <influous@mailbox.org>
2024-04-27 00:20:10 +0100

    Initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,12 @@
-// module.exports = function (eleventyConfig) {
-//   eleventyConfig.setBrowserSyncConfig({
-//     files: "./**/*.css", // "./public/static/**/*.css"
-//   });
-
-//   return {
-//     dir: {
-//       input: "src",
-//       output: "public",
-//     },
-//   };
-// };
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
-    notify: true,
+    files: "./public/static/**/*.css",
   });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
